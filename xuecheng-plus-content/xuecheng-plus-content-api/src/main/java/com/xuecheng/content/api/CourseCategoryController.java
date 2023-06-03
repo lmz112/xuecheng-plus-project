b86478bd5812557576d3a2fe5ba47107fe516ecf
lmz <limz1121@163.com>
2023-06-03 10:12:25 +0800

    课程分类接口完毕

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseCategoryController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseCategoryController.java
@@ -1,8 +1,12 @@
 package com.xuecheng.content.api;
 
 import com.xuecheng.content.model.dto.CourseCategoryTreeDto;
+import com.xuecheng.content.service.CourseCategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * 课程分类相关接口
@@ -10,9 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CourseCategoryController {
 
+    @Autowired
+    CourseCategoryService courseCategoryService;
+
     @GetMapping("/course-category/tree-nodes")
-    public CourseCategoryTreeDto queryTreeNodes(){
-        return null;
+    public List<CourseCategoryTreeDto> queryTreeNodes(){
+        return courseCategoryService.queryTreeNodes("1");
     }
 
 }
--- xuecheng-plus-content/xuecheng-plus-content-model/src/main/java/com/xuecheng/content/model/dto/CourseCategoryTreeDto.java
+++ xuecheng-plus-content/xuecheng-plus-content-model/src/main/java/com/xuecheng/content/model/dto/CourseCategoryTreeDto.java
@@ -2,6 +2,7 @@ package com.xuecheng.content.model.dto;
 
 import com.xuecheng.content.model.po.CourseCategory;
 import lombok.Data;
+import lombok.ToString;
 
 import java.util.List;
 
@@ -9,7 +10,8 @@ import java.util.List;
  *
  */
 @Data
-public class CourseCategoryTreeDto extends CourseCategory implements java.io.Serializable {
+@ToString
+public class CourseCategoryTreeDto extends CourseCategory  {
 
-    List<CourseCategoryTreeDto> childrenTreeNodes;
+    private List<CourseCategoryTreeDto> childrenTreeNodes;
 }
